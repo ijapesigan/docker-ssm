f256b4ddf99ccc50da1e0dd744f320c993d66e66
ijapesigan <ijapesigan@gmail.com>
2024-03-11 02:19:42 +0000

    Automated build.

---
+++ scripts/ctsem.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# ctsem dependencies
+install2.r --error --skipinstalled -n -1 \
+    cOde                                 \
+    data.table                           \
+    Deriv                                \
+    expm                                 \
+    ggplot2                              \
+    MASS                                 \
+    Matrix                               \
+    mize                                 \
+    mvtnorm                              \
+    plyr                                 \
+    RcppParallel                         \
+    rstan                                \
+    rstantools                           \
+    tibble                               \
+    BH                                   \
+    Rcpp                                 \
+    RcppEigen                            \
+    RcppParallel                         \
+    StanHeaders
+
+# ctsem
+install2.r --error --skipinstalled -n -1 \
+    ctsem
+
+echo -e "\nInstall ctsem, done!"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+bash ubuntu_cran_pkg.sh
 bash rproject.sh
 bash dynr_sys.sh
 bash dynr.sh
+bash ctsem.sh
 bash simstatespace.sh
 bash post.sh
