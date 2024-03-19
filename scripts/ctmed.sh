ef5d919be3958b7e855d40a56127f3e856da22a3
ijapesigan <ijapesigan@gmail.com>
2024-03-19 07:09:11 +0000

    Automated build.

--- scripts/ctmed.sh
+++ scripts/ctmed.sh
@@ -4,7 +4,7 @@ set -e
 
 R -e "remotes::install_github(      \
     c(                              \
-        'jeksterslab/CTMed'      \
+        'jeksterslab/cTMed'         \
     )                               \
 )"
 
